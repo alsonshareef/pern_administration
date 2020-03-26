04957cdf1f6ebd39f71bfb6aada174f35440ca8e
Alson Shareef <alson.shareef@gmail.com>
2020-03-26 18:12:37 +0800

    moved homepage route to routes folder and changed color of server listen handler.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 
 dotenv.config({ path: '../config/config.env' });
 
+const homepageRoute = require('./routes/homepage');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use('/api/v1/home', homepageRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${port}`.yellow.bold
+  )
 );
