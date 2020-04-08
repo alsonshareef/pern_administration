c0e78611e6003860caccd141c3a7d2b51ec9ce6c
Alson Shareef <alson.shareef@gmail.com>
2020-04-08 19:52:28 +0800

    Create new student route

---
+++ server/routes/students.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../database');
+
+router.get('/', (req, res) => {
+  res.send('Students page');
+});
+
+// Create a new student.
+router.post('/', async (req, res) => {
+  try {
+    const { first_name, last_name, email } = req.body;
+    const newStudent = await db.query(
+      'INSERT INTO students (first_name, last_name, email) values ($1, $2, $3) RETURNING *',
+      [first_name, last_name, email]
+    );
+
+    res.json(newStudent.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+module.exports = router;
