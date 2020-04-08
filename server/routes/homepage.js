e732a403e79fba95b05de768dbe050896656a772
Alson Shareef <alson.shareef@gmail.com>
2020-04-08 19:51:38 +0800

    More meaningful return text.

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the homepage.');
+  res.send('API root page.');
 });
 
 module.exports = router;
