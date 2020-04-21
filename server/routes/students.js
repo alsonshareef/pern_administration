4588be8296d1a0248499ca3b87edd6a1641eefe1
Alson Shareef <alson.shareef@gmail.com>
2020-04-21 21:01:34 +0800

    adding password column to student table

--- server/routes/students.js
+++ server/routes/students.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const db = require('../database');
 
+// Students homepage.
 router.get('/', (req, res) => {
   res.send('Students page');
 });
@@ -10,10 +11,10 @@ router.get('/', (req, res) => {
 // Create a new student.
 router.post('/', async (req, res) => {
   try {
-    const { first_name, last_name, email } = req.body;
+    const { first_name, last_name, email, pass } = req.body;
     const newStudent = await db.query(
-      'INSERT INTO student (first_name, last_name, email) values ($1, $2, $3) RETURNING *',
-      [first_name, last_name, email]
+      'INSERT INTO students (first_name, last_name, email, pass) values ($1, $2, $3, $4) RETURNING *',
+      [first_name, last_name, email, pass]
     );
 
     res.json(newStudent.rows[0]);
