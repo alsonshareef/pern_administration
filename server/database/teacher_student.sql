14fb6286af3913cdb4048a7424d78801fc6e3845
Alson Shareef <alson.shareef@gmail.com>
2020-06-03 18:13:37 +0800

    Added password as fourth column of data for both students and teachers tables.

--- server/database/teacher_student.sql
+++ server/database/teacher_student.sql
@@ -1,23 +1,31 @@
--- Students and Teachers tables.
-create table student (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  email VARCHAR(100) NOT NULL
-);
+-- STUDENT / TEACHER TABLES.
 
-create table teacher (
+/* 
+-- Table relationships
+  1. ONE student can be registered to MANY teachers.
+  2. ONE teacher can register MANY students.
+*/
+
+create table students (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(100) NOT NULL,
-  registered_students_id BIGINT REFERENCES student (id)
+  pass VARCHAR(128) NOT NULL,
+);
+
+create table teachers (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  pass VARCHAR(128) NOT NULL,
+  registered_students_id BIGINT REFERENCES students (id)
 );
 
 -- Placeholder data
-insert into student (first_name, last_name, email) values ('John', 'Doe', 'john.doe@test.com');
-insert into student (first_name, last_name, email) values ('Bill', 'Bob', 'bill.bob@test.com');
-insert into student (first_name, last_name, email) values ('Alson', 'Shareef', 'alson.shareef@test.com');
+insert into students (first_name, last_name, email, pass) values ('John', 'Doe', 'john.doe@test.com', 'test');
+insert into students (first_name, last_name, email, pass) values ('Bill', 'Bob', 'bill.bob@test.com', 'test');
 
-insert into teacher (first_name, last_name, email) values ('English', 'Teacher', 'english.teacher@test.com');
-insert into teacher (first_name, last_name, email) values ('Math', 'Teacher', 'math.teacher@test.com');
+insert into teachers (first_name, last_name, email, pass) values ('English', 'Teacher', 'english.teacher@test.com', 'test');
+insert into teachers (first_name, last_name, email, pass) values ('Math', 'Teacher', 'math.teacher@test.com', 'test');
