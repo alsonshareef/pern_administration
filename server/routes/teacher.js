0737c7a5200fcd040ebf25d1f74bfbe452b620b0
Alson Shareef <alson.shareef@gmail.com>
2020-09-16 00:45:56 +0800

    Teacher routes with respective controller methods.

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({
-    message: 'Teachers homepage.',
-  });
-});
+const teacherRoutes = require('../controllers/teacher');
+
+// TEACHER HOMEPAGE
+router.get('/', teacherRoutes.getTeacherHomepage);
+
+// CREATE - Register a new teacher.
+router.post('/createteacher', teacherRoutes.postCreateTeacher);
+
+// CREATE - Register one or more students to a specified teacher.
+router.post('/registerstudents', teacherRoutes.postRegisterStudent);
+
+// READ - Retrieve a list of students common to a given list of teachers.
+router.get('/commonstudents', teacherRoutes.getCommonStudents);
+
+// UPDATE - Suspend a specified student.
+router.post('/suspend', teacherRoutes.postSuspendStudent);
+
+// READ - Retrieve a list of students who are eligible to receieve notifications.
+router.post(
+  '/retrievefornotifications',
+  teacherRoutes.postRetrieveNotifications
+);
 
 module.exports = router;
