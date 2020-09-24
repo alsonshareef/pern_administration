d4768fcfaf80550e57c31cb43a777740cc8a79a4
Alson Shareef <alson.shareef@gmail.com>
2020-09-24 22:15:45 +0800

    Finished getCommonStudents controller method.

--- server/controllers/teacher.js
+++ server/controllers/teacher.js
@@ -4,12 +4,16 @@
 
 const pool = require('../utils/db');
 
+const getOccurence = require('../utils/get_occurence');
+
+// Teacher homepage after login.
 exports.getTeacherHomepage = (req, res, next) => {
   res.json({
     message: 'Teachers homepage.',
   });
 };
 
+// ** MIGHT NOT NEED TO BE NEEDED AS TEACHER AUTHORIZED FUNCTIONALITY ** - Register a new teacher.
 exports.postCreateTeacher = async (req, res, next) => {
   try {
     const { firstName, lastName, email, password } = req.body;
@@ -24,12 +28,26 @@ exports.postCreateTeacher = async (req, res, next) => {
   }
 };
 
-exports.postRegisterStudent = (req, res, next) => {};
+// Register a student or multiple students to a specified teacher.
+exports.postRegisterStudent = async (req, res, next) => {
+  try {
+    /**
+     * 1. Retrieve student-to-be-registered's data from req.body and check to see if this student already exists.
+     * 2. If student exists, grab the student id from students table and teacher id from teachers table and insert both ID's into registration table.
+     * 3. If student doesn't exist, add the student-to-be-registered into the students table and continue with step 2.
+     */
+    const students = await pool.query('SELECT * FROM students');
+    res.json(students);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
+// Retrieve all the common students between a given list of teachers.
 exports.getCommonStudents = async (req, res, next) => {
   try {
     const registrations = await pool.query(
-      `SELECT s.student_first_name, s.student_last_name, t.teacher_first_name, t.teacher_last_name
+      `SELECT s.id, s.student_first_name, s.student_last_name, s.student_email, t.teacher_first_name, t.teacher_last_name, t.teacher_email
       FROM students s
       INNER JOIN registrations r 
       ON s.id = r.student_id
@@ -37,17 +55,55 @@ exports.getCommonStudents = async (req, res, next) => {
       ON t.id = r.teacher_id;`
     );
 
-    const commonStudents = registrations.rows.filter((reg) => {
-      // Eventually filter out students based on teacher authentication.
-      return reg.teacher_last_name === 'Jimmy';
-    });
+    // Total students
+    const students = await pool.query('SELECT student_email from students');
 
-    res.json(commonStudents);
+    // Specified teachers retrieved from client.
+    const specifiedTeachers =
+      typeof req.query.teachers == 'string'
+        ? [req.query.teachers]
+        : req.query.teachers;
+    console.log('teachers:', specifiedTeachers);
+
+    let commonStudents = [];
+    let registeredStudents = [];
+
+    if (specifiedTeachers.length > 1) {
+      // Gather all registered students including duplicates
+      specifiedTeachers.forEach((teacher) => {
+        registrations.rows.forEach((reg) => {
+          if (teacher == reg.teacher_email) {
+            registeredStudents.push(reg.student_email);
+          }
+        });
+      });
+
+      // Look through duplicate registered students and find which students appear more than once to find common students.
+      students.rows.forEach((student) => {
+        if (getOccurence(registeredStudents, student.student_email) > 1) {
+          commonStudents.push(student.student_email);
+        }
+      });
+    } else {
+      // If only one teacher is specified, display students registered under this one teacher.
+      registrations.rows.forEach((reg) => {
+        if (reg.teacher_email == specifiedTeachers[0]) {
+          commonStudents.push(reg.student_email);
+        }
+      });
+    }
+
+    if (commonStudents.length !== 0) {
+      res.json({ commonStudents });
+    }
+    res.json('There are no common students between the specified teachers.');
   } catch (err) {
     console.error(err.message);
   }
 };
 
+// Suspend a student.
 exports.postSuspendStudent = (req, res, next) => {};
 
+// Retrieve a list of students who are authorized to receive a notification.
 exports.postRetrieveNotifications = (req, res, next) => {};
