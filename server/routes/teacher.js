cdd790afe50e36f6c34346c21896f43db5873d65
Alson Shareef <alson.shareef@gmail.com>
2020-10-20 20:59:05 +0800

    Renamed teacher controller methods which are now retrieved from deconstructing.

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const teacherController = require('../controllers/teacher');
+const {
+	getTeacherHomepage,
+	registerStudents,
+	unregisterStudents,
+	getCommonStudents,
+	suspendStudents,
+} = require('../controllers/teacher');
 
-// TEACHER HOMEPAGE
-router.get('/', teacherController.getTeacherHomepage);
+// READ - Retrieve the logged in teacher's homepage.
+router.get('/', getTeacherHomepage);
 
 // CREATE - Register one or more students to a specified teacher.
-router.post('/registerstudents', teacherController.postRegisterStudent);
-
-// READ - Retrieve a list of students common to a given list of teachers.
-router.get('/commonstudents', teacherController.getCommonStudents);
-
-// UPDATE - Suspend a specified student.
-router.post('/suspend', teacherController.postSuspendStudent);
+router.post('/register', registerStudents);
 
 // DELETE - Unregister one or more students from specified teachers.
-router.post('/unregister', teacherController.unregisterStudents);
+router.delete('/unregister', unregisterStudents);
+
+// READ - Retrieve a list of students common to a given list of teachers.
+router.get('/commonstudents', getCommonStudents);
+
+// UPDATE - Suspend specified students.
+router.post('/suspendstudent', suspendStudents);
 
 module.exports = router;
